02cc7b2ce8cc21917dc9f685cd9fe622dedc7387
U-O-Nanakusa724 <u.o.nanakusa724.engineer@gmail.com>
2021-01-30 16:38:24 +0900

    [add]api-1 Swagger導入
    アプリの土台作成

---
+++ cucp_api/src/test/java/biz/uoray/cucp_api/CucpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.uoray.cucp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
