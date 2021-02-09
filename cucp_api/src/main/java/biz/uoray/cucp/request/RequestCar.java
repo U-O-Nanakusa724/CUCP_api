0f3dbc5a67d62df1aa2c1e946d0fc93faa44dc68
“U-O-Nanakusa724“ <u.o.nanakusa724.engineer@gmail.com>
2021-02-09 11:04:05 +0900

    [clear]api-11 Responseのアノテーション整理
    不要なアノテーションを除去

--- cucp_api/src/main/java/biz/uoray/cucp/request/RequestCar.java
+++ cucp_api/src/main/java/biz/uoray/cucp/request/RequestCar.java
@@ -1,6 +1,5 @@
 package biz.uoray.cucp.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.annotations.ApiModelProperty;
