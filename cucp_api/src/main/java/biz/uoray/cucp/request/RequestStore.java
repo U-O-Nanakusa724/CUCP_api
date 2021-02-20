a3efd4c042b2f455a0decd43487c3c0dbf04b15c
rayboc <rayboc0803@gmail.com>
2021-02-21 02:05:05 +0900

    [feature/api-3]StoreContorollerのAPI化
    RequestStoreクラスのnameフィールドに「min = 1」バリデーション値追加

--- cucp_api/src/main/java/biz/uoray/cucp/request/RequestStore.java
+++ cucp_api/src/main/java/biz/uoray/cucp/request/RequestStore.java
@@ -1,11 +1,12 @@
 package biz.uoray.cucp.request;
 
+import javax.validation.constraints.Size;
+
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-
-import javax.validation.constraints.Size;
 
 @Data
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
@@ -14,7 +15,7 @@ public class RequestStore {
     @ApiModelProperty("ID")
     int id;
 
-    @Size(max = 64)
+    @Size(min = 1, max = 64)
     @ApiModelProperty("販売店名")
     String name;
 }
