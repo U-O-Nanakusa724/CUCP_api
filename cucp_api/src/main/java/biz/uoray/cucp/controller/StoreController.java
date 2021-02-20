9e9f0bbad2ac022e2cc3aba17a98ea7bfc31ee3d
rayboc <rayboc0803@gmail.com>
2021-02-21 01:47:52 +0900

    [feature/api-3]StoreContorollerのAPI化
    Storeコントローラー、サービスのAPI化、Responseクラス作成

--- cucp_api/src/main/java/biz/uoray/cucp/controller/StoreController.java
+++ cucp_api/src/main/java/biz/uoray/cucp/controller/StoreController.java
@@ -1,119 +1,107 @@
-//package biz.uoray.cucp.controller;
-//
-//import biz.uoray.cucp.form.StoreForm;
-//import biz.uoray.cucp.service.StoreService;
-//import biz.uoray.common.entity.Store;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.validation.BindingResult;
-//import org.springframework.validation.annotation.Validated;
-//import org.springframework.web.bind.annotation.*;
-//
-//@Controller
-//@RequestMapping("/v1/stores")
-//public class StoreController {
-//
-//    @Autowired
-//    StoreService storeService;
-//
-//    /**
-//     * 販売店一覧表示
-//     *
-//     * @param model
-//     * @return
-//     */
-//    @GetMapping()
-//    public String getStore(Model model, Pageable pageable) {
-//        Page<Store> storeEntityPage = storeService.getAll(pageable);
-//        model.addAttribute("pages", storeEntityPage);
-//        model.addAttribute("storeList", storeEntityPage.getContent());
-//        model.addAttribute("url", "/stores");
-//        return "store/index";
-//    }
-//
-//    /**
-//     * 販売店新規登録画面表示
-//     *
-//     * @return
-//     */
-//    @GetMapping("/new")
-//    public String createStore(StoreForm storeForm) {
-//        return "store/new";
-//    }
-//
-//    /**
-//     * 販売店新規登録実行
-//     *
-//     * @param storeForm
-//     * @param bindingResult
-//     * @return
-//     */
-//    @PostMapping("/create")
-//    public String insertStore(@Validated StoreForm storeForm,
-//                              BindingResult bindingResult) {
-//
-//        if (bindingResult.hasErrors()) {
-//            return "store/new";
-//        }
-//
-//        storeService.createStore(storeForm.getName());
-//        return "redirect:/stores";
-//    }
-//
-//    /**
-//     * 販売店編集画面表示
-//     *
-//     * @param model
-//     * @param id
-//     * @return
-//     */
-//    @GetMapping("/{id}/edit")
-//    public String editStore(Model model,
-//                            StoreForm storeForm,
-//                            @PathVariable("id") Integer id) {
-//        Store store = storeService.getActiveStoreById(id);
-//        storeForm.setName(store.getName());
-//        model.addAttribute("storeId", store.getId());
-//        model.addAttribute("store", storeForm);
-//
-//        return "store/edit";
-//    }
-//
-//    /**
-//     * 販売店編集実行
-//     *
-//     * @param id
-//     * @param storeForm
-//     * @return
-//     */
-//    @PutMapping("/{id}/update")
-//    public String updateStore(Model model,
-//                              @PathVariable("id") Integer id,
-//                              @Validated StoreForm storeForm,
-//                              BindingResult bindingResult) {
-//        if (bindingResult.hasErrors()) {
-//            model.addAttribute("storeId", id);
-//            model.addAttribute("store", storeForm);
-//            return "store/edit";
-//        }
-//
-//        storeService.updateStore(id, storeForm.getName());
-//        return "redirect:/stores";
-//    }
-//
-//    /**
-//     * 販売店１件削除
-//     *
-//     * @param id
-//     * @return
-//     */
-//    @DeleteMapping("/{id}/delete")
-//    public String deleteStore(@PathVariable("id") Integer id) {
-//
-//        storeService.deleteStore(id);
-//        return "redirect:/stores";
-//    }
-//}
+package biz.uoray.cucp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import biz.uoray.cucp.request.RequestStore;
+import biz.uoray.cucp.response.ResponseStore;
+import biz.uoray.cucp.response.ResponseStoreList;
+import biz.uoray.cucp.service.StoreService;
+import io.swagger.annotations.ApiOperation;
+
+@CrossOrigin
+@Controller
+@RequestMapping("/v1/stores")
+public class StoreController {
+
+	@Autowired
+	StoreService storeService;
+
+	/**
+	 * 販売店一覧表示
+	 *
+	 * @param model
+	 * @return
+	 */
+	@ResponseBody
+	@ApiOperation(value = "販売店一覧を取得する", nickname = "getStores")
+	@GetMapping(produces = "application/json")
+	public ResponseStoreList getStore() throws Exception {
+		Pageable pageable = PageRequest.of(0, 20);
+		return new ResponseStoreList(storeService.getAll(pageable));
+	}
+
+	/**
+	 * 販売店新規登録
+	 *
+	 * @param requestStrore
+	 * @return
+	 * @throws Exception
+	 */
+	@ResponseBody
+	@ApiOperation(value = "販売店を登録する", nickname = "createStores")
+	@PostMapping("/create")
+	public ResponseStore postStore(@RequestBody RequestStore requestStrore) throws Exception {
+		return new ResponseStore(storeService.createStore(requestStrore));
+	}
+
+	/**
+	 * 販売店編集
+	 *
+	 * @param requestStrore
+	 * @return
+	 * @throws Exception
+	 */
+	@ResponseBody
+	@ApiOperation(value = "販売店を編集する", nickname = "updateStores")
+	@PutMapping("/update")
+	public ResponseStore putStore(@Validated @RequestBody RequestStore requestStrore) throws Exception {
+		return new ResponseStore(storeService.updateStore(requestStrore));
+	}
+
+	/**
+	 * 販売店1件削除
+	 *
+	 * @param id
+	 * @return
+	 * @throws Exception
+	 */
+	@ResponseBody
+	@ApiOperation(value = "販売店を削除する", nickname = "deleteStores")
+	@DeleteMapping("/{id}/delete")
+	public ResponseEntity<Void> deleteStore(@PathVariable("id") Integer id) throws Exception {
+		storeService.deleteStore(id);
+		return ResponseEntity.ok().build();
+	}
+
+	/**
+	 * 販売店検索
+	 *
+	 * @param select
+	 * @param keyword
+	 * @return
+	 * @throws Exception
+	 */
+	@ResponseBody
+	@ApiOperation(value = "販売店を検索する", nickname = "searchStores")
+	@GetMapping("/search")
+	public ResponseStoreList searchStore(@RequestParam("select") String select,
+			                             @RequestParam("keyword") String keyword) throws Exception {
+		Pageable pageable = PageRequest.of(0, 20);
+		return new ResponseStoreList(storeService.searchStore(pageable, select, keyword));
+	}
+}
