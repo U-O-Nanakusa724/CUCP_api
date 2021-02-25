f32a0977ee13fbdac64801405eec7bc702832b23
“U-O-Nanakusa724“ <u.o.nanakusa724.engineer@gmail.com>
2021-02-25 14:10:06 +0900

    [clear]api-3 StoreAPI
    リファクタ

--- cucp_api/src/main/java/biz/uoray/cucp/controller/StoreController.java
+++ cucp_api/src/main/java/biz/uoray/cucp/controller/StoreController.java
@@ -1,5 +1,6 @@
 package biz.uoray.cucp.controller;
 
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -26,82 +27,82 @@ import io.swagger.annotations.ApiOperation;
 @CrossOrigin
 @Controller
 @RequestMapping("/v1/stores")
+@Api(value = "販売店コントローラ", tags = "StoreController", produces = "application/json")
 public class StoreController {
 
-	@Autowired
-	StoreService storeService;
+    @Autowired
+    StoreService storeService;
 
-	/**
-	 * 販売店一覧表示
-	 *
-	 * @param model
-	 * @return
-	 */
-	@ResponseBody
-	@ApiOperation(value = "販売店一覧を取得する", nickname = "getStores")
-	@GetMapping(produces = "application/json")
-	public ResponseStoreList getStore() throws Exception {
-		Pageable pageable = PageRequest.of(0, 20);
-		return new ResponseStoreList(storeService.getAll(pageable));
-	}
+    /**
+     * 販売店一覧表示
+     *
+     * @return
+     */
+    @ResponseBody
+    @ApiOperation(value = "販売店一覧を取得する", nickname = "getStores")
+    @GetMapping(produces = "application/json")
+    public ResponseStoreList getStore() throws Exception {
+        Pageable pageable = PageRequest.of(0, 20);
+        return new ResponseStoreList(storeService.getAll(pageable));
+    }
 
-	/**
-	 * 販売店新規登録
-	 *
-	 * @param requestStrore
-	 * @return
-	 * @throws Exception
-	 */
-	@ResponseBody
-	@ApiOperation(value = "販売店を登録する", nickname = "createStores")
-	@PostMapping("/create")
-	public ResponseStore postStore(@RequestBody RequestStore requestStrore) throws Exception {
-		return new ResponseStore(storeService.createStore(requestStrore));
-	}
+    /**
+     * 販売店新規登録
+     *
+     * @param requestStore
+     * @return
+     * @throws Exception
+     */
+    @ResponseBody
+    @ApiOperation(value = "販売店を登録する", nickname = "createStores")
+    @PostMapping("/create")
+    public ResponseStore postStore(@RequestBody RequestStore requestStore) throws Exception {
+        return new ResponseStore(storeService.createStore(requestStore));
+    }
 
-	/**
-	 * 販売店編集
-	 *
-	 * @param requestStrore
-	 * @return
-	 * @throws Exception
-	 */
-	@ResponseBody
-	@ApiOperation(value = "販売店を編集する", nickname = "updateStores")
-	@PutMapping("/update")
-	public ResponseStore putStore(@Validated @RequestBody RequestStore requestStrore) throws Exception {
-		return new ResponseStore(storeService.updateStore(requestStrore));
-	}
+    /**
+     * 販売店編集
+     *
+     * @param requestStore
+     * @return
+     * @throws Exception
+     */
+    @ResponseBody
+    @ApiOperation(value = "販売店を編集する", nickname = "updateStores")
+    @PutMapping("/update")
+    public ResponseStore putStore(@Validated @RequestBody RequestStore requestStore) throws Exception {
+        return new ResponseStore(storeService.updateStore(requestStore));
+    }
 
-	/**
-	 * 販売店1件削除
-	 *
-	 * @param id
-	 * @return
-	 * @throws Exception
-	 */
-	@ResponseBody
-	@ApiOperation(value = "販売店を削除する", nickname = "deleteStores")
-	@DeleteMapping("/{id}/delete")
-	public ResponseEntity<Void> deleteStore(@PathVariable("id") Integer id) throws Exception {
-		storeService.deleteStore(id);
-		return ResponseEntity.ok().build();
-	}
+    /**
+     * 販売店1件削除
+     *
+     * @param id
+     * @return
+     * @throws Exception
+     */
+    @ResponseBody
+    @ApiOperation(value = "販売店を削除する", nickname = "deleteStores")
+    @DeleteMapping("/{id}/delete")
+    public ResponseEntity<Void> deleteStore(@PathVariable("id") Integer id) throws Exception {
+        storeService.deleteStore(id);
+        return ResponseEntity.ok().build();
+    }
 
-	/**
-	 * 販売店検索
-	 *
-	 * @param select
-	 * @param keyword
-	 * @return
-	 * @throws Exception
-	 */
-	@ResponseBody
-	@ApiOperation(value = "販売店を検索する", nickname = "searchStores")
-	@GetMapping("/search")
-	public ResponseStoreList searchStore(@RequestParam("select") String select,
-			                             @RequestParam("keyword") String keyword) throws Exception {
-		Pageable pageable = PageRequest.of(0, 20);
-		return new ResponseStoreList(storeService.searchStore(pageable, select, keyword));
-	}
+    /**
+     * 販売店検索
+     *
+     * @param select
+     * @param keyword
+     * @return
+     * @throws Exception
+     */
+    @ResponseBody
+    @ApiOperation(value = "販売店を検索する", nickname = "searchStores")
+    @GetMapping("/search")
+    public ResponseStoreList searchStore(@RequestParam("select") String select,
+                                         @RequestParam("keyword") String keyword) throws Exception {
+        Pageable pageable = PageRequest.of(0, 20);
+        return new ResponseStoreList(storeService.searchStore(pageable, select, keyword));
+    }
 }
